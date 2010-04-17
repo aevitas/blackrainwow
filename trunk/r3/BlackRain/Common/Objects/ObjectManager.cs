2230d357b2434ae8722952086d23585f28d74bec
m4rtijn <m4rtijn@69fa205e-2839-fb78-76d7-efe58f1f849e>
2010-04-17 10:35:15 +0000

    See changelog for changes.

---
+++ trunk/r3/BlackRain/Common/Objects/ObjectManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using BlackRain.Constants;
+using Magic;
+
+namespace BlackRain.Common.Objects
+{
+    public static class ObjectManager
+    {
+        public static BlackMagic Memory { get; set; }
+        public static Process WowProcess { get; set; }
+        public static WowPlayer Me { get; private set; }
+        public static bool Initialized { get; private set; }
+
+        #region <Initialization Fields>
+        private static uint CConnectionPointer { get; set; }
+        private static uint CConnection { get; set; }
+        private static uint CConnectionOffset { get; set; }
+        private static string TLSPattern { get { return "EB 02 33 C0 8B D 00 00 00 00 64 8B 15 00 00 00 00 8B 34 8A 8B D 00 00 00 00 89 81 00 00 00 00"; } }
+        private static string TLSMask { get { return "xxxxxx????xxx????xxxxx????xx????"; } }
+        private static uint ThreadLocalStorage { get; set; }
+        private static uint CurrentManager { get; set; }
+        #endregion
+        #region <Properties>
+        public static ulong LocalGUID { get; set; }
+        #endregion
+
+        public static List<WowObject> Objects  = new List<WowObject>();
+
+        /// <summary>
+        /// Initializes the ObjectManager, attaching it to the selected process.
+        /// </summary>
+        /// <param name="pid">The World of Warcraft process ID.</param>
+        public static void Initialize(int pid)
+        {
+            if (Memory != null || Initialized) return;
+                Memory = new BlackMagic(pid); // Assign Memory to the selected Wow instance.
+
+            try
+            {
+                ThreadLocalStorage = Memory.FindPattern(TLSPattern, TLSMask);
+
+                if (ThreadLocalStorage != uint.MaxValue) // Ensure we have found the TLS.
+                {
+                    CConnection = Memory.ReadUInt(Memory.ReadUInt(ThreadLocalStorage + 0x16));
+                    CConnectionOffset = Memory.ReadUInt(ThreadLocalStorage + 0x1C);
+                    CurrentManager = Memory.ReadUInt(CConnection + CConnectionOffset);
+
+                    Logging.Write(string.Format("CurrentManager found at {0:X}", CurrentManager));
+
+                    // Retrieve the local player's GUID for later use. (This is a dodgy way to go about things.)
+                    LocalGUID = Memory.ReadUInt64(CurrentManager + (uint)Offsets.ObjectManager.LocalGUID);
+
+                    if (CurrentManager != uint.MinValue && CurrentManager != uint.MaxValue)
+                    {
+                        Initialized = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.Write(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Pulses the ObjectManager, refreshing any objects it holds.
+        /// <!-- This is a slow way to go around things, but it's accurate, and that's what counts. -->
+        /// </summary>
+        public static void Pulse()
+        {
+            if (Objects.Count != int.MinValue)
+                Objects.Clear(); // Clean up any previous objects in the list, if we have them.
+
+            try
+            {
+                var currentObject = new WowObject(Memory.ReadUInt(CurrentManager + (uint)Offsets.ObjectManager.FirstObject));
+
+                while (currentObject.BaseAddress != uint.MinValue && currentObject.BaseAddress % 2 == uint.MinValue)
+                {
+                    switch (currentObject.Type)
+                    {
+                        case (int)Offsets.WowObjectType.Unit:
+                            Objects.Add(new WowUnit(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.Item:
+                            Objects.Add(new WowItem(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.Container:
+                            Objects.Add(new WowContainer(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.Corpse:
+                            Objects.Add(new WowCorpseObject(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.GameObject:
+                            Objects.Add(new WowGameObject(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.DynamicObject:
+                            Objects.Add(new WowDynamicObject(currentObject.BaseAddress));
+                            break;
+                        case (int)Offsets.WowObjectType.Player:
+                            Objects.Add(new WowPlayer(currentObject.BaseAddress));
+                            break;
+                    }
+
+                    if (currentObject.GUID == LocalGUID)
+                        Me = new WowPlayer(currentObject.BaseAddress);
+
+                    currentObject.BaseAddress = Memory.ReadUInt(currentObject.BaseAddress + (uint)Offsets.ObjectManager.NextObject);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logging.Write("Exception in ObjectManager: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ trunk/r3/BlackRain/Common/Objects/WowUnit.cs
@@ -0,0 +1,221 @@
+﻿using BlackRain.Constants;
+
+namespace BlackRain.Common.Objects
+{
+    public class WowUnit : WowObject
+    {
+        public WowUnit(uint baseAddress)
+            : base(baseAddress)
+        {
+        }
+
+        /// <summary>
+        /// Is this unit a critter?
+        /// </summary>
+        public bool Critter
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_CRITTER) == 1 ? true : false; }
+        }
+
+        /// <summary>
+        /// The GUID of the object this unit is charmed by.
+        /// </summary>
+        public ulong CharmedBy
+        {
+            get { return GetStorageField<ulong>((uint) Offsets.WowUnitFields.UNIT_FIELD_CHARMEDBY); }
+        }
+
+        /// <summary>
+        /// The GUID of the object this unit is summoned by.
+        /// </summary>
+        public ulong SummonedBy
+        {
+            get { return GetStorageField<ulong>((uint) Offsets.WowUnitFields.UNIT_FIELD_SUMMONEDBY); }
+        }
+
+        /// <summary>
+        /// The GUID of the object this unit was created by.
+        /// </summary>
+        public ulong CreatedBy
+        {
+            get { return GetStorageField<ulong>((uint) Offsets.WowUnitFields.UNIT_FIELD_CREATEDBY); }
+        }
+
+        /// <summary>
+        /// The unit's health.
+        /// </summary>
+        public int Health
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_HEALTH); }
+        }
+
+        /// <summary>
+        /// The unit's maximum health.
+        /// </summary>
+        public int MaximumHealth
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXHEALTH); }
+        }
+
+        /// <summary>
+        /// The unit's health as a percentage of its total.
+        /// </summary>
+        public int HealthPercentage
+        {
+            get { return (100 * Health) / MaximumHealth; }
+        }
+
+        /// <summary>
+        /// The unit's base health.
+        /// </summary>
+        public int BaseHealth
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_BASE_HEALTH); }
+        }
+
+        /// <summary>
+        /// The unit's base health.
+        /// </summary>
+        public int BaseMana
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_BASE_MANA); }
+        }
+
+        /// <summary>
+        /// The unit's mana.
+        /// </summary>
+        public int Mana
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER1); }
+        }
+
+        /// <summary>
+        /// The unit's rage.
+        /// </summary>
+        public int Rage
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER2); }
+        }
+
+        /// <summary>
+        /// The unit's focus.
+        /// </summary>
+        public int Focus
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER3); }
+        }
+
+        /// <summary>
+        /// The unit's energy.
+        /// </summary>
+        public int Energy
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER4); }
+        }
+
+        /// <summary>
+        /// The unit's happinnes.
+        /// </summary>
+        public int Happinnes
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER4); }
+        }
+
+        /// <summary>
+        /// The unit's runic power.
+        /// </summary>
+        public int RunicPower
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER5); }
+        }
+
+        /// <summary>
+        /// The unit's runes.
+        /// </summary>
+        public int Runes
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_POWER6); }
+        }
+
+        /// <summary>
+        /// The unit's maximum mana.
+        /// </summary>
+        public int MaximumMana
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER1); }
+        }
+
+        /// <summary>
+        /// The unit's maximum rage.
+        /// </summary>
+        public int MaximumRage
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER2); }
+        }
+
+        /// <summary>
+        /// The unit's maximum focus.
+        /// </summary>
+        public int MaximumFocus
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER3); }
+        }
+
+        /// <summary>
+        /// The unit's maximum energy.
+        /// </summary>
+        public int MaximumEnergy
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER4); }
+        }
+
+        /// <summary>
+        /// The unit's maximum runic power.
+        /// </summary>
+        public int MaximumRunicPower
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER5); }
+        }
+
+        /// <summary>
+        /// The unit's maximum runes.
+        /// <!-- This is presumably always 6, two blood, two frost, two unholy. But may be different on DK based bosses/mobs? -->
+        /// </summary>
+        public int MaximumRunes
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowUnitFields.UNIT_FIELD_MAXPOWER6); }
+        }
+
+        /// <summary>
+        /// The unit's level.
+        /// </summary>
+        public int Level
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_LEVEL); }
+        }
+
+        /// <summary>
+        /// The unit's DisplayID.
+        /// </summary>
+        public int DisplayID
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_DISPLAYID); }
+        }
+
+        /// <summary>
+        /// The mount display of the mount the unit is mounted on.
+        /// </summary>
+        public int MountDisplayID
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowUnitFields.UNIT_FIELD_MOUNTDISPLAYID); }
+        }
+
+        /// <summary>
+        /// The GUID of the object this unit is targeting.
+        /// </summary>
+        public ulong Target
+        {
+            get { return GetStorageField<ulong>((uint) Offsets.WowUnitFields.UNIT_FIELD_TARGET); }
+        }
+    }
+}
