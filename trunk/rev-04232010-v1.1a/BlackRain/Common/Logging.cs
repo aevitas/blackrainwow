c8d0834ec71acf189fb2a1ed7899513b0d788b8d
m4rtijn <m4rtijn@69fa205e-2839-fb78-76d7-efe58f1f849e>
2010-04-23 12:56:57 +0000

    04232010
    Descriptors working again. (Oh yeah, *= doesn't work!)
    ObjectManager optimized.

---
+++ trunk/rev-04232010-v1.1a/BlackRain/Common/Logging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace BlackRain.Common
+{
+    /// <summary>
+    /// Handles all generic logging for BlackRain.
+    /// </summary>
+    public static class Logging
+    {
+        /// <summary>
+        /// The Prefix for the BlackRain log filename.
+        /// </summary>
+        private const string Prefix = "BlackRain_";
+
+        /// <summary>
+        /// Writes to the general output log.
+        /// </summary>
+        /// <param name="sender">The object the log entry came from.</param>
+        /// <param name="value">The value to be logged.</param>
+        /// <returns>true if successful, otherwise false.</returns>
+        public static bool Write(object sender, string value)
+        {
+            using (var writer = new StreamWriter(Prefix + "Debug.html", true))
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    writer.WriteLine("[" + DateTime.Now + "] - " + value + " - From: " + sender + "<br/>");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Writes to the general output log.
+        /// </summary>
+        /// <param name="value">The value to be logged.</param>
+        /// <returns>true if successful, otherwise false.</returns>
+        public static bool Write(string value)
+        {
+            using (var writer = new StreamWriter(Prefix + "Debug.html", true))
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    writer.WriteLine("[" + DateTime.Now + "] - " + value + "<br/>");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ trunk/rev-04232010-v1.1a/BlackRain/Common/Objects/WowGameObject.cs
@@ -0,0 +1,78 @@
+﻿using BlackRain.Constants;
+
+namespace BlackRain.Common.Objects
+{
+    public class WowGameObject : WowObject
+    {
+        public WowGameObject(uint baseAddress)
+            : base(baseAddress)
+        {
+        }
+
+        /// <summary>
+        /// The GameObject's Display ID.
+        /// </summary>
+        public int DisplayID
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowGameObjectFields.GAMEOBJECT_DISPLAYID); }
+        }
+
+        /// <summary>
+        /// The GameObject's faction.
+        /// </summary>
+        public int Faction
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowGameObjectFields.GAMEOBJECT_FACTION); }
+        }
+
+        /// <summary>
+        /// The GameObject's level.
+        /// </summary>
+        public new int Level
+        {
+            get { return GetStorageField<int>((uint) Offsets.WowGameObjectFields.GAMEOBJECT_LEVEL); }
+        }
+
+        /// <summary>
+        /// The GUID of the object this object was created by.
+        /// <!-- Presumably, hasn't been double-checked. -->
+        /// </summary>
+        public ulong CreatedBy
+        {
+            get { return GetStorageField<ulong>((uint) Offsets.WowGameObjectFields.OBJECT_FIELD_CREATED_BY); }
+        }
+
+        /// <summary>
+        /// Returns the GameObject's X position.
+        /// </summary>
+        public override float X
+        {
+            get
+            {
+                return ObjectManager.Memory.ReadFloat(BaseAddress + (uint) Offsets.WowObject.GameObjectX);
+            }
+        }
+
+        /// <summary>
+        /// Returns the GameObject's Y position.
+        /// </summary>
+        public override float Y
+        {
+            get
+            {
+                return ObjectManager.Memory.ReadFloat(BaseAddress + (uint) Offsets.WowObject.GameObjectY);
+            }
+        }
+
+        /// <summary>
+        /// Returns the GameObject's Z position.
+        /// </summary>
+        public override float Z
+        {
+            get
+            {
+                return ObjectManager.Memory.ReadFloat(BaseAddress + (uint) Offsets.WowObject.GameObjectZ);
+            }
+        }
+    }
+}
