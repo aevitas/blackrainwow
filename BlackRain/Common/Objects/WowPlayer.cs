d5d785b964d63df00a2f1998f39fb515fa7cbeca
m4rtijn <m4rtijn@69fa205e-2839-fb78-76d7-efe58f1f849e>
2010-10-15 14:29:17 +0000

    Updated to be compatible with 4.0.1. Credits to Ddebug's patch @ MMOwned.

---
+++ BlackRain/Common/Objects/WowPlayer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace BlackRain.Common.Objects
+{
+    /// <summary>
+    /// Represents a player.
+    /// </summary>
+    public class WowPlayer : WowUnit
+    {
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="baseAddress"></param>
+        public WowPlayer(uint baseAddress)
+            : base (baseAddress)
+        {
+        }
+
+        /// <summary>
+        /// The player's experience points.
+        /// </summary>
+        public int Experience
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowPlayerFields.PLAYER_XP); }
+        }
+
+        /// <summary>
+        /// The experience the player requires to advance to the next level.
+        /// </summary>
+        public int NextLevel
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowPlayerFields.PLAYER_NEXT_LEVEL_XP); }
+        }
+
+        /// <summary>
+        /// The ID of the guild the player resides in.
+        /// </summary>
+        public int GuildID
+        {
+            get { return /*GetStorageField<int>((uint)Offsets.WowPlayerFields.PLAYER_GUILDID)*/ 0; }
+        }
+
+        /// <summary>
+        /// The amount of experience the player has rested.
+        /// <!-- You can calculate the double experience rate, and thus shorten the time to the next level when using this in a calculation. -->
+        /// </summary>
+        public int RestExperience
+        {
+            get { return GetStorageField<int>((uint)Offsets.WowPlayerFields.PLAYER_REST_STATE_EXPERIENCE); }
+        }
+
+        /// <summary>
+        /// Determines if the player is a Ghost. i.e: dead, and released from corpse.
+        /// </summary>
+        public bool Ghost
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Returns true if in combat, false if not.
+        /// </summary>
+        public bool Combat
+        {
+            get { return HasUnitFlag(Offsets.UnitFlags.Combat); }
+        }
+
+        public override string Name
+        {
+            get {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the unit is mounted, false if not.
+        /// </summary>
+        public bool Mounted
+        {
+            get
+            {
+                return MountDisplayID > 0;
+            }
+        }
+    }
+}
